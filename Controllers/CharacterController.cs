59a3fc9ef6188fc96bf1ced4c54b8ee5292c2396
CharKourkoulis <cult_bob@hotmail.com>
2021-03-12 19:22:53 +0200

    second commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 1, Name = "Conan" }
+        };
+
+
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id) {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+
+    }
+}
